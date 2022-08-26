e292fd34876f2900da27e25dd4d6db192b3aff78
ivanFlor3s <ifloresmanc96@gmail.com>
2022-08-25 21:18:58 -0300

    ejercicio 19

---
+++ src/Test/HelloWord/ejercicio19.java
@@ -0,0 +1,40 @@
+package Test.HelloWord;
+
+public class ejercicio19 {
+
+	public static void main(String[] args) {
+		
+		
+		MyHelper helper = new MyHelper(); 
+		
+		boolean esInpar = false;
+		boolean esDeUnSoloDigito = false;
+		boolean noEstaEnNinguno = false;
+		boolean estaEnAmbosGrupos = false;
+		
+		int numero;
+		numero = helper.printAndAskForNumber("Ingrese un numero entero");
+		
+		esInpar = (numero%2 != 0);
+		esDeUnSoloDigito = numero >= 0 && numero < 10;
+		estaEnAmbosGrupos = esInpar && noEstaEnNinguno;
+		noEstaEnNinguno = !esInpar && !esDeUnSoloDigito;
+		
+		if (esInpar) {
+			helper.print("El numero es inpar");
+		}
+		if (esDeUnSoloDigito) {
+			helper.print("El numero es de un Ssolo digito");
+		}
+		if(estaEnAmbosGrupos) {
+			helper.print("El numero esta en ambos grupos, es muy popular");
+		}
+		if (noEstaEnNinguno) {
+			helper.print("Es un pobre bastardo");
+		}
+		
+		helper.asesinarScanner();
+		
+	}
+
+}

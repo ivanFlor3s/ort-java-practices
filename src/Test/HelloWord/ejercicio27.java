177f768d2292bf129a7058af20c8b1e605d1fd68
ivanFlor3s <ifloresmanc96@gmail.com>
2022-08-25 22:14:04 -0300

    ejericio 27

---
+++ src/Test/HelloWord/ejercicio27.java
@@ -0,0 +1,30 @@
+package Test.HelloWord;
+
+public class ejercicio27 {
+
+	static final int CANT_EDADES = 5;
+	static final int EDAD_MAYOR = 18;
+	
+	public static void main(String[] args) {
+	
+		MyHelper helper = new MyHelper();
+		int auxEdad;
+		int acumuladorEdades = 0;
+		int acumMayoresImpares = 0;
+		
+		for (int i = 0; i < CANT_EDADES; i++) {
+			auxEdad = helper.printAndAskForNumber("Ingrese una edad");
+			acumuladorEdades += auxEdad;
+			if(auxEdad > EDAD_MAYOR && helper.esImpar(auxEdad) ) {
+				acumMayoresImpares++;
+			}
+		}
+		
+		helper.print("El promedio entre las " + CANT_EDADES + " edades es: " + ((double)acumuladorEdades / CANT_EDADES));
+		helper.print("Se ingresaron " + acumMayoresImpares + " edades mayor a " + EDAD_MAYOR + " e impares");
+		
+		helper.asesinarScanner();
+
+	}
+
+}

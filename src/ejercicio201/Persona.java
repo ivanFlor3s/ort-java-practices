eea11ee6c6c822a5a59d6215bd42ea8f5e165bf8
ivanFlor3s <ifloresmanc96@gmail.com>
2022-10-07 15:04:38 -0300

    creo ejercicio 2 y clase persona

---
+++ src/ejercicio201/Persona.java
@@ -0,0 +1,15 @@
+package ejercicio201;
+
+public class Persona {
+	private String nombre;
+	private String apellido;
+	
+	
+	public void ponerNombre(String unNombre) {
+		this.nombre = unNombre;
+	}
+	
+	public void ponerApellido(String unApellid) {
+		this.apellido= unApellid;
+	}
+}

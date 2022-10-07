22dbc2ba25d6d534a97eb2ff4068c0d77479a37e
ivanFlor3s <ifloresmanc96@gmail.com>
2022-10-07 15:12:12 -0300

    Ej 4 Instanciando Personas

--- src/ejercicio201/Ejercicio2.java
+++ src/ejercicio201/Ejercicio2.java
@@ -1,10 +1,32 @@
 package ejercicio201;
 
+import Test.HelloWord.MyHelper;
+
 public class Ejercicio2 {
-
+	
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//INSTANCIO UNA PERSONA
+		Persona p = new Persona();
+		
+		MyHelper h = new MyHelper();
+		
+		p.ponerNombre("Julio");
+		p.ponerApellido("Strasera");
+		
+		h.print(p.obtenerNombreCompleto());
+		
+		Persona p2= new Persona();
+		
+		p2.ponerNombre("Dieguito");
+		p2.ponerApellido("Marodano");
+		h.print(p2.obtenerNombreCompleto());
+		
+		
+		
+		
 	}
-
+	
+	//El uml te lo debo
 }
